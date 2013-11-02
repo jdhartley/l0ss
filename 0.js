27b404ebfa1fb307174beca06d4ce2bfa26b730e
JD Hartley <jdhrtl@gmail.com>
2013-11-02 18:05:43 -0500

    Glitch only sideways; glitch each indiv

--- 0.js
+++ 0.js
@@ -2,66 +2,58 @@ $(function()
 {
   console.log('blast.off!');
 
-  var $h = $('h1').html(function()
+  $('h1 span').each(function()
   {
-    var $span = $('<span>').text( $(this).text() );
-    return [$span.clone(), $span];
-  }),
-
-    $e = $h.find('span').last(),
-
-    height = $e.height(),
-    width = $e.width();
-
-  $e.css({
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    height: height,
-    width: width,
-    overflow: 'hidden',
-    'background-color': '#000'
-  });
-
-  var rnd=function(a)
-  {
-    return Math.floor(Math.random()*(a+1));
-  },
-
-  rnda = function(a)
-  {
-    return Math.random() < .5 ? rnd(a) : rnd(a * -1);
-  },
-
-  g = function()
-  {
-    for ( var i = 0; i <= rnd( 5 ); i++ )
+    var $h = $(this).html(function()
     {
-      setTimeout(function()
+      var $span = $('<span>').text( $(this).text() );
+      return [$span.clone(), $span];
+    }),
+
+      $e = $h.find('span').last(),
+
+      height = $e.height(),
+      width = $e.width();
+
+    $e.css({
+      position: 'absolute',
+      height: height,
+      width: width
+    });
+
+    var rnd=function(a)
+    {
+      return Math.floor(Math.random()*(a+1));
+    },
+
+    rnda = function(a)
+    {
+      return Math.random() < .5 ? rnd(a) : rnd(a * -1);
+    },
+
+    g = function()
+    {
+      for ( var i = 0; i <= rnd( 5 ); i++ )
       {
-        $e.css( Math.random() < 0.5
-          ? {
-            top: rnda( 6 ),
-            width: rnd( Math.floor( width * .8 ) ) + 2,
-            height: height
-          }
-          : {
+        setTimeout(function()
+        {
+          $e.css({
             left: rnda( 6 ),
             height: rnd( Math.floor( height * .8 ) ) + 2,
             width: width
-          }
-        );
-      }, rnd( 300 ));
+          });
+        }, rnd( 300 ));
+      }
+
+      setTimeout(function()
+      {
+        $e.css({ left: 0, top: 0 })
+      }, 340);
+
+      setTimeout( g, ( rnd(4) + 1 ) * 200 );
     }
 
-    setTimeout(function()
-    {
-      $e.css({ left: 0, top: 0 })
-    }, 340);
+    g();
 
-    setTimeout( g, ( rnd(4) + 1 ) * 200 );
-  }
-
-  g();
-
+  });
 });
