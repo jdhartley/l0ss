8b004e1df7f04411a9f724cb46bd601081ebcd3d
JD Hartley <jdhrtl@gmail.com>
2013-11-02 20:26:21 -0500

    get photos from json and append to body:

--- 0.js
+++ 0.js
@@ -1,52 +1,67 @@
-~(function(L0SS) {
+~(function(L0SS)
+{
+	var glitchy = function()
+	{
+		$('<h1><span>project</span> <span>L0SS</span></h1>').appendTo('body').find('span').each(function()
+		{
+			var $h = $(this).html(function()
+				{
+					var text = $(this).text();
+					return '<span>' + text + '</span><span>' + text + '</span>';
+				}),
 
-  L0SS.glitchy = function()
-  {
-    $('<h1><span>project</span> <span>L0SS</span></h1>').appendTo('body').find('span').each(function() {
-      var $h = $(this).html(function() {
-        var $span = $('<span>').text( $(this).text() );
-        return [$span.clone(), $span];
-      }),
+				$e = $h.find('span').last(),
 
-        $e = $h.find('span').last(),
+				height = $e.height(),
+				width = $e.width(),
+				rnd = function( a ) { return Math.floor( Math.random() * ( a + 1 ) ) },
+				rnda = function( a ) { return Math.random() < .5 ? rnd(a) : rnd(a * -1) },
 
-        height = $e.height(),
-        width = $e.width();
+				glitch = function()
+				{
+					for ( var i = 0; i <= rnd( 5 ); i++ )
+					{
+						setTimeout(function()
+						{
+							$e.css({
+								left: rnda( 6 ),
+								height: rnd( Math.floor( height * .8 ) ) + 2,
+								width: width
+							});
+						}, rnd( 300 ));
+					}
 
-      $e.css({
-        position: 'absolute',
-        height: height,
-        width: width
-      });
+					setTimeout(function() { $e.css({ left: 0, top: 0 }) }, 340);
+					setTimeout( glitch, ( rnd(4) + 1 ) * 200 );
+				};
 
-      var rnd = function( a ) { return Math.floor( Math.random() * ( a + 1 ) ) },
-          rnda = function( a ) { return Math.random() < .5 ? rnd(a) : rnd(a * -1) },
+			$e.css({
+				position: 'absolute',
+				height: height,
+				width: width
+			});
 
-          glitch = function() {
-            for ( var i = 0; i <= rnd( 5 ); i++ ) {
-              setTimeout(function() {
-                $e.css({
-                  left: rnda( 6 ),
-                  height: rnd( Math.floor( height * .8 ) ) + 2,
-                  width: width
-                });
-              }, rnd( 300 ));
-            }
+			glitch();
 
-            setTimeout(function() { $e.css({ left: 0, top: 0 }) }, 340);
-            setTimeout( glitch, ( rnd(4) + 1 ) * 200 );
-          };
+		});
+	},
 
-      glitch();
+	matrix = function(photos)
+	{
+		photos.forEach(function(data)
+		{
+			$('<img/>').attr('src', data.src).appendTo('body');
+		});
+	};
 
-    });
-  }
+	$(function()
+	{
+		// glitchy();
 
-  $(function() {
-    L0SS.glitchy();
+		$.getJSON('/data.json', matrix);
 
-  })
+	})
 
-}( window.L0SS || ( window.L0SS = {} ) ));
+}());
 
 document.title = 'project L0SS';
